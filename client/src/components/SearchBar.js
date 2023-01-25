cd42f5b1ff432d73f45f4ab38755a01743c74d9a
Nicholas Edwards <edwards.nicholas1997@gmail.com>
2023-01-25 09:54:09 -0500

    Building out account center, messaging center; changed appointments location in offcanvases.

--- client/src/components/SearchBar.js
+++ client/src/components/SearchBar.js
@@ -6,7 +6,6 @@ function SearchBar() {
         <>
         <Form id="search-bar">
             <Form.Control type="search" placeholder="Search for lawyers by location"/>
-            <Form.Text></Form.Text>
         </Form>
         </>
     );
