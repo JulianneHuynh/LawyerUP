acbbbd6fcc2ee1c70f54fb6c1f1cb07c6ba3ba63
Julianne Huynh <JulianneHuynh@yahoo.com>
2023-01-25 08:54:14 -0600

    signups

---
+++ client/src/components/SignupLawyer.js
@@ -0,0 +1,113 @@
+import React,  {useState} from 'react'
+import {useHistory} from 'react-router-dom'
+// import {Form} from '../styled/Form'
+
+function SignupLawyer() {
+  const [formData, setFormData] = useState({
+      name:'',
+      username:'',
+      email:'',
+      specialty:'',
+      law_firm:'',
+      alma_mater:'',
+      years_in_practice:'',
+      location:'',
+      password:'',
+  })
+  const [errors, setErrors] = useState([])
+  const history = useHistory()
+
+  const {name, username, email, specialty, law_firm, alma_mater, years_in_practice, location, password} = formData
+
+  function onSubmit(e){
+    e.preventDefault()
+    const lawyer = {
+      name,
+      username,
+      email, 
+      specialty,
+      law_firm,
+      alma_mater,
+      years_in_practice,
+      location,
+      password, 
+    }
+
+    fetch(`/lawyers`,{
+      method:'POST',
+      headers:{'Content-Type': 'application/json'},
+      body:JSON.stringify(lawyer)
+    })
+    .then(res => {
+      if(res.ok){
+        res.json().then(lawyer => {
+          history.push(`/lawyers/${lawyer.id}`)
+        })
+      }else {
+        res.json().then(json => setErrors(Object.entries(json.errors)))
+      }
+    })
+
+  }
+
+  const handleChange = (e) => {
+    const { username, value } =e.target
+    setFormData({ ...formData, [username]: value})
+  }
+  return (
+    <>
+    <Form onSubmit={onSubmit}>
+
+      <label>
+        Name
+      </label>
+      <input placeholder='Jane Doe'  type='text' legal_name='name' value={name} onChange={handleChange} />
+
+      <label>
+        Username 
+      </label>
+      <input placeholder='janedoe123' type='text' legal_name='username' value={username} onChange={handleChange} />
+
+      <label>
+        Email
+      </label>
+      <input placeholder='janedoe123@gmail.com' type='text' email='email' value={email} onchange={handleChange} />
+
+      <label>
+        Specialty
+      </label>
+      <input type='text' specialty='specialty' value={specialty} onchange={handleChange} />
+
+      <label>
+        Law Firm
+      </label>
+      <input placeholder='Akerman LLP' type='text' law_firm='law_firm' value={law_firm} onchange={handleChange} />
+
+      <label>
+        Alma Mater
+      </label>
+      <input placeholder='New York University' type='text' alma_mater='alma_mater' value={alma_mater} onchange={handleChange} />
+
+      <label>
+        Years of Experience
+      </label>
+      <input type='text' years_in_practice='years_in_practice' value={years_in_practice} onchange={handleChange} />
+
+      <label>
+        Location
+      </label>
+      <input placeholder='Zip Code' type='text' location='location' value={location} onchange={handleChange} />
+
+      <label>
+        Password
+      </label> 
+      <input type='text' password='password' value={password} onChange={handleChange} />
+
+      <input type= 'submit' value ='Sign Up!' />
+    </Form>
+    {errors&&<div>{errors}</div>}
+    </>
+  )
+}
+
+export default SignupLawyer;
